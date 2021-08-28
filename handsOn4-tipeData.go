c0d32999b3f3e07ff851395056d2cb6de8fea687
Annis Nuraini <annisnuraini.93@gmail.com>
2021-08-28 16:54:49 +0700

    add new file handsOn4-tipeData.go

---
+++ handsOn4-tipeData.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	i := 42
+	f := 3.142
+	g := 0.867 + 0.5i
+
+	fmt.Printf("i mempunyai tipe data %T\n", i)
+	fmt.Printf("f mempunyai tipe data %T\n", f)
+	fmt.Printf("g mempunyai tipe data %T\n", g)
+}
